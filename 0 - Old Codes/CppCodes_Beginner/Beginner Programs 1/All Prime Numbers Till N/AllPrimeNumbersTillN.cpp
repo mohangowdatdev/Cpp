f055303b43279d91bd93ec5b04632a7ed45cdb36
Mohan Gowda T <mohangowdat.dev@gmail.com>
2022-06-18 10:01:50 +0530

    mm

--- 0 - Old Codes/CppCodes_Beginner/Beginner Programs 1/All Prime Numbers Till N/AllPrimeNumbersTillN.cpp
+++ 0 - Old Codes/CppCodes_Beginner/Beginner Programs 1/All Prime Numbers Till N/AllPrimeNumbersTillN.cpp
@@ -9,16 +9,6 @@ int main(){
     for(int i=2;i<=n;i++)
     {
         isprime = 1;
-        //num = i;
-        /*div=2;
-        while(div<i){ 
-            if(i%div==0){
-                isprime=false;
-                break;
-            }
-            div=div+1;
-        } */
-
         for(int div=2;div<i;div++)
             if(i%div==0)
                 isprime = 0;
