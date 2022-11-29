53c167c2702edba8e6f55e88ccf17f2adb119c11
Mohan Gowda T <mohangowdat.dev@gmail.com>
2022-11-29 22:49:56 +0530

    some more commits

---
+++ Patterns/pattern-3/pattern3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+    cout << "Number : ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout << j << " ";
+            Sleep(50);
+        }
+        Sleep(500);
+        cout << "\n";
+    }
+}
